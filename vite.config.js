2ed410fcaf186631bcddcc4e41768477b298d567
Pleo2 <leooel23m@gmail.com>
2024-11-15 01:44:12 -0400

    first commit

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import MillionLint from '@million/lint';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [MillionLint.vite({
+    enabled: true
+  }), react()],
+})
