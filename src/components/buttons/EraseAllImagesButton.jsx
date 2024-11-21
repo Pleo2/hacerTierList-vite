0531a55d130665b4979cc853b832bd52ed534e57
Pleo2 <leooel23m@gmail.com>
2024-11-20 21:03:56 -0400

    add ErraseAllImages, resetImages

--- src/components/buttons/EraseAllImagesButton.jsx
+++ src/components/buttons/EraseAllImagesButton.jsx
@@ -1,17 +1,24 @@
+import { ImagesContext } from "../../context/ImagesContext";
+// --------------------------------------------------------
 import { EraserIcon } from "@radix-ui/react-icons";
+import { useContext } from "react";
 import { memo } from "react";
 
+
 export const EraserAllImagesButton = memo(() => {
-	// Call to Erase All data
+	const {setImages} = useContext(ImagesContext)
+	
+	const eraseImages = () => {
+		setImages([])
+	}
+	
 
 	return (
 		<>
 			<div className="dark:bg-gray-900 bg-gray-200 border-b-4 border-redOrange">
 				<button
 					type="button"
-					onClick={() => {
-						console.log(cleanImages);
-					}}
+					onClick={eraseImages}
 				>
 					<EraserIcon />
 				</button>
--- src/components/buttons/RestoreButton.jsx
+++ src/components/buttons/RestoreButton.jsx
@@ -1,10 +1,33 @@
 import { ReloadIcon } from "@radix-ui/react-icons";
+import { ImagesContext } from "../../context/ImagesContext";
+import { useContext } from "react";
 
 export const RestoreButton = () => {
+	const { setImages } = useContext(ImagesContext);
+
+	const restoreImages = () => {
+		setImages((prev) => {
+			const arr = []
+
+			for (const element of prev) {
+				if (element.rowId !== 5) {
+					arr.push({
+						rowId: 5,
+						img: element.img,
+					});
+				} else {
+					arr.push(element);
+				}
+			}
+
+			return arr;
+		});
+	};
+
 	return (
 		<>
 			<div className="dark:bg-gray-900 bg-gray-200 border-b-4 border-purple">
-				<button type="button">
+				<button type="button" onClick={restoreImages}>
 					<ReloadIcon />
 				</button>
 			</div>
